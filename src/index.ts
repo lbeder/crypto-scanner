f1b07c4719616c7db53a6d71e37bb2d64d114118
lbeder <lbeder@users.noreply.github.com>
2022-12-24 01:49:42 +0000

    Refactor to yargs commands

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import yargs from "yargs/yargs";
+
 import crypto from "crypto";
 import Web3 from "web3";
 import Decimal from "decimal.js";
@@ -8,43 +9,21 @@ import { WebsocketProvider } from "web3-core";
 import { AbiItem } from "web3-utils";
 import inquirer from "inquirer";
 
-import { config } from "dotenv";
-
 import ERC20_API from "./erc20-abi.json";
 
-config();
-
-const {
-  argv: { getConfig }
-} = yargs(process.argv).option("getConfig", {
-  alias: "g",
-  type: "boolean",
-  description: "Get config"
-});
-
-const { WEB3_PROVIDER = "ws://localhost:8545" } = process.env;
-
 const ETH = new Decimal(10 ** 18);
-const web3 = new Web3(WEB3_PROVIDER);
 
-const getBalance = async (address: string): Promise<Decimal> => {
-  const balance = await web3.eth.getBalance(address);
-  return new Decimal(balance.toString()).div(ETH);
-};
+interface Device {
+  description: string;
+  addresses: string[];
+}
 
-const getTokenBalance = async (address: string, token: string, decimals: number): Promise<Decimal> => {
-  const tokenContract = new web3.eth.Contract(ERC20_API as AbiItem[], token);
-  const balance = await tokenContract.methods.balanceOf(address).call();
-
-  return new Decimal(balance.toString()).div(10 ** decimals);
-};
-
-export interface Totals {
+interface Totals {
   ETH: Decimal;
   [additionalProperties: string]: Decimal;
 }
 
-export interface DeviceTotals {
+interface DeviceTotals {
   [device: string]: Totals;
 }
 
@@ -77,80 +56,137 @@ const decryptConfig = (password: string) => {
 
 // tslint:disable: no-console
 const main = async () => {
+  let web3: Web3;
+  let password;
+  let addresses: Device[];
+  let tokens: [];
+
+  const getBalance = async (address: string): Promise<Decimal> => {
+    const balance = await web3.eth.getBalance(address);
+    return new Decimal(balance.toString()).div(ETH);
+  };
+
+  const getTokenBalance = async (address: string, token: string, decimals: number): Promise<Decimal> => {
+    const tokenContract = new web3.eth.Contract(ERC20_API as AbiItem[], token);
+    const balance = await tokenContract.methods.balanceOf(address).call();
+
+    return new Decimal(balance.toString()).div(10 ** decimals);
+  };
+
   try {
-    const { password } = await inquirer.prompt([
-      {
-        type: "password",
-        name: "password",
-        message: "Enter password"
-      }
-    ]);
-
-    const { addresses, tokens } = decryptConfig(password);
-
-    if (getConfig) {
-      console.log(addresses);
-      console.log(tokens);
-
-      return;
-    }
-
-    const totals: Totals = { ETH: new Decimal(0) };
-    const deviceTotals: DeviceTotals = {};
-
-    for (const device of addresses) {
-      console.log("Processing", device.description);
-
-      deviceTotals[device.description] = { ETH: new Decimal(0) };
-      const deviceTotal = deviceTotals[device.description];
-
-      for (const address of device.addresses) {
-        const ethBalance = await getBalance(address);
-        totals.ETH = totals.ETH.add(ethBalance);
-        deviceTotal.ETH = deviceTotal.ETH.add(ethBalance);
-
-        for (const token of tokens) {
-          const { address: tokenAddress, symbol, decimals } = token;
-          if (totals[symbol] === undefined) {
-            totals[symbol] = new Decimal(0);
+    await yargs(process.argv.slice(2))
+      .option("provider", {
+        type: "string",
+        default: "ws://localhost:8545",
+        description: "Web3 provider's URL"
+      })
+      .option("verbose", {
+        type: "boolean",
+        description: "Verbose mode"
+      })
+      .middleware(({ provider }) => {
+        web3 = new Web3(provider);
+      })
+      .middleware(async () => {
+        ({ password } = await inquirer.prompt([
+          {
+            type: "password",
+            name: "password",
+            message: "Enter password"
           }
-          if (deviceTotal[symbol] === undefined) {
-            deviceTotal[symbol] = new Decimal(0);
+        ]));
+
+        ({ addresses, tokens } = decryptConfig(password));
+      })
+      .command(
+        "config",
+        "Get the config",
+        () => {}, // tslint:disable-line: no-empty
+        () => {
+          console.log(addresses);
+          console.log(tokens);
+
+          console.log();
+        }
+      )
+      .command(
+        "fetch",
+        "Fetch the data",
+        () => {}, // tslint:disable-line: no-empty
+        async ({ verbose }) => {
+          const totals: Totals = { ETH: new Decimal(0) };
+          const deviceTotals: DeviceTotals = {};
+
+          for (const device of addresses) {
+            const { description, addresses: deviceAddresses } = device;
+            console.log("Processing", description);
+
+            deviceTotals[description] = { ETH: new Decimal(0) };
+            const deviceTotal = deviceTotals[description];
+
+            for (const address of deviceAddresses) {
+              const ethBalance = await getBalance(address);
+              if (verbose && !ethBalance.isZero()) {
+                console.log(address, ethBalance, "ETH");
+              }
+
+              totals.ETH = totals.ETH.add(ethBalance);
+              deviceTotal.ETH = deviceTotal.ETH.add(ethBalance);
+
+              for (const token of tokens) {
+                const { address: tokenAddress, symbol, decimals } = token;
+                if (totals[symbol] === undefined) {
+                  totals[symbol] = new Decimal(0);
+                }
+                if (deviceTotal[symbol] === undefined) {
+                  deviceTotal[symbol] = new Decimal(0);
+                }
+
+                const tokenBalance = new Decimal((await getTokenBalance(address, tokenAddress, decimals)).toString());
+                if (verbose && !tokenBalance.isZero()) {
+                  console.log(address, tokenBalance, symbol);
+                }
+
+                totals[symbol] = totals[symbol].add(tokenBalance);
+                deviceTotal[symbol] = deviceTotal[symbol].add(tokenBalance);
+              }
+            }
+
+            console.log("");
           }
 
-          const tokenBalance = new Decimal((await getTokenBalance(address, tokenAddress, decimals)).toString());
-          totals[symbol] = totals[symbol].add(tokenBalance);
-          deviceTotal[symbol] = deviceTotal[symbol].add(tokenBalance);
+          console.log("");
+          console.log("Totals:");
+          Object.entries(totals).forEach(([symbol, value]) => {
+            if (!value.isZero()) {
+              console.log(`${symbol}: ${Number(value.toFixed()).toLocaleString()}`);
+            }
+          });
+
+          console.log("");
+          console.log("Device Totals:");
+          Object.entries(deviceTotals).forEach(([description, devTotals]) => {
+            console.log(description);
+
+            Object.entries(devTotals).forEach(([symbol, value]) => {
+              if (!value.isZero()) {
+                console.log(`${symbol}: ${Number(value.toFixed()).toLocaleString()}`);
+              }
+            });
+
+            console.log("");
+          });
         }
-      }
-    }
-
-    console.log("");
-    console.log("Totals:");
-    Object.entries(totals).forEach(([symbol, value]) => {
-      if (!value.isZero()) {
-        console.log(`${symbol}: ${Number(value.toFixed()).toLocaleString()}`);
-      }
-    });
-
-    console.log("");
-    console.log("Device Totals:");
-    Object.entries(deviceTotals).forEach(([description, devTotals]) => {
-      console.log(description);
-
-      Object.entries(devTotals).forEach(([symbol, value]) => {
-        if (!value.isZero()) {
-          console.log(`${symbol}: ${Number(value.toFixed()).toLocaleString()}`);
-        }
-      });
-
-      console.log("");
-    });
+      )
+      .demandCommand()
+      .onFinishCommand(() => {
+        (web3.currentProvider as WebsocketProvider)?.disconnect(0, "OK");
+      })
+      .help()
+      .parse();
   } catch (e) {
     console.error(e);
   }
-
-  (web3.currentProvider as WebsocketProvider)?.disconnect(0, "OK");
 };
 
 main();
