988528d39ac757c2ab128afd0fcc1bcb94cf9623
lbeder <lbeder@users.noreply.github.com>
2022-12-24 01:49:42 +0000

    Refactoring, updates, and addition of totals summary

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,69 @@
-import BigNumber from "bignumber.js";
-const Web3 = require("web3");
-const ERC20_API = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_newOwnerCandidate", "type": "address" }], "name": "requestOwnershipTransfer", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "isMinting", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_amount", "type": "uint256" }], "name": "mint", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "acceptOwnership", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [{ "name": "", "type": "address" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "newOwnerCandidate", "outputs": [{ "name": "", "type": "address" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_tokenAddress", "type": "address" }, { "name": "_amount", "type": "uint256" }], "name": "transferAnyERC20Token", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" }], "name": "allowance", "outputs": [{ "name": "remaining", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "endMinting", "outputs": [], "payable": false, "type": "function" }, { "anonymous": false, "inputs": [], "name": "MintingEnded", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "_by", "type": "address" }, { "indexed": true, "name": "_to", "type": "address" }], "name": "OwnershipRequested", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "_from", "type": "address" }, { "indexed": true, "name": "_to", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }];
-const ETH = new BigNumber(10 ** 18);
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+import Decimal from "decimal.js";
+import Web3 from "web3";
+import { AbiItem } from "web3-utils";
+import { config } from "dotenv";
 
-const getTokenBalance = async (address: string, token: string, decimals: number): Promise<BigNumber> => {
-  const tokenContract = new web3.eth.Contract(ERC20_API, token);
+import DEVICES from "./addresses-eth.json";
+import TOKENS from "./tokens-eth.json";
+import ERC20_API from "./erc20-abi.json";
+
+config();
+
+const { WEB3_PROVIDER = "http://localhost:8545" } = process.env;
+
+const ETH = new Decimal(10 ** 18);
+const web3 = new Web3(new Web3.providers.HttpProvider(WEB3_PROVIDER));
+
+const getBalance = async (address: string): Promise<Decimal> => {
+  const balance = await web3.eth.getBalance(address);
+  return new Decimal(balance.toString()).div(ETH);
+};
+
+const getTokenBalance = async (address: string, token: string, decimals: number): Promise<Decimal> => {
+  const tokenContract = new web3.eth.Contract(ERC20_API as AbiItem[], token);
   const balance = await tokenContract.methods.balanceOf(address).call();
-  return new BigNumber(balance).div(10 ** decimals);
+
+  return new Decimal(balance.toString()).div(10 ** decimals);
 };
 
-const getBalance = async (address: string): Promise<BigNumber> => {
-  return new BigNumber(await web3.eth.getBalance(address)).div(ETH);
-};
-
-interface Device {
-  description: string;
-  addresses: string[];
+export interface Totals {
+  ETH: Decimal;
+  [additionalProperties: string]: Decimal;
 }
 
-interface Token {
-  symbol: string;
-  address: string;
-  decimals: number;
-}
-
-const DEVICES: Device[] = require("../addresses-eth.json");
-const TOKENS: Token[] = require("../tokens-eth.json");
-
+// tslint:disable: no-console
 const main = async () => {
-  for (let i = 0; i < DEVICES.length; ++i) {
-    const device = DEVICES[i];
+  const totals: Totals = { ETH: new Decimal(0) };
 
+  for (const device of DEVICES) {
     console.log(device.description);
 
-    for (let j = 0; j < device.addresses.length; ++j) {
-      const address = device.addresses[j];
-      console.log(`  ${address}`);
-      console.log("    ETH:", (await getBalance(address)).toFormat());
+    for (const address of device.addresses) {
+      const ethBalance = await getBalance(address);
+      totals.ETH = totals.ETH.add(ethBalance);
 
-      for (let k = 0; k < TOKENS.length; ++k) {
-        const token = TOKENS[k];
-        console.log(`    ${token.symbol}:`, (await getTokenBalance(address, token.address, token.decimals)).toFormat());
+      console.log(`  ${address}`);
+      console.log("    ETH:", Number(ethBalance.toFixed()).toLocaleString());
+
+      for (const token of TOKENS) {
+        const { address: tokenAddress, symbol, decimals } = token;
+        if (totals[symbol] === undefined) {
+          totals[symbol] = new Decimal(0);
+        }
+
+        const tokenBalance = new Decimal((await getTokenBalance(address, tokenAddress, decimals)).toString());
+        console.log(`    ${symbol}:`, Number(tokenBalance.toFixed()).toLocaleString());
+        totals[symbol] = totals[symbol].add(tokenBalance);
       }
 
       console.log("");
     }
   }
+
+  console.log("Totals:");
+  Object.entries(totals).forEach(([symbol, value]) =>
+    console.log(`${symbol}: ${Number(value.toFixed()).toLocaleString()}`)
+  );
 };
 
 main();
