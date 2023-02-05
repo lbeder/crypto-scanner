72a80a1ec0f08b7c334da5533e422d32cc3248ab
lbeder <lbeder@users.noreply.github.com>
2023-02-05 21:58:12 +0000

    Create the data dir if it doesn't exist

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -25,6 +25,10 @@ export class Config {
   constructor(password: string) {
     this.password = password;
 
+    if (!fs.existsSync(DATA_DIR)) {
+      fs.mkdirSync(DATA_DIR, { recursive: true });
+    }
+
     if (!fs.existsSync(CONFIG_PATH)) {
       this.data = {
         ledgers: {},
